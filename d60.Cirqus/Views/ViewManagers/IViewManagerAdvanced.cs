80f77e68bf45795c879160cb11810ed061a56882
irium <irium@mail.ru>
2015-12-17 02:28:26 +0300

    Added IViewManagerAdvanced interface with #GetViewIds() method for diagnostics purposes.

---
+++ d60.Cirqus/Views/ViewManagers/IViewManagerAdvanced.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace d60.Cirqus.Views.ViewManagers
+{
+    /// <summary>
+    /// Advanced API for a view manager
+    /// </summary>
+    public interface IViewManagerAdvanced
+    {
+        /// <summary>
+        /// Returns ids of existing view instances.
+        /// </summary>
+        IEnumerable<string> GetViewIds();
+    }
+}
